90bbf69d4b3ddc907d3e04f3db3019f95760aea0
s30695 <s30695@pjwstk.edu.pl>
2025-03-09 23:00:47 +0100

    Zmieniono nazwę zmiennej sum na total w metodzie CalculateAverage

--- Program.cs
+++ Program.cs
@@ -3,16 +3,15 @@
 public static class MathHelper
 {
     public static double CalculateAverage(int[] numbers)
+{
+    double total = 0;
+    foreach (var num in numbers)
     {
-        if (numbers == null || numbers.Length == 0)
-            throw new ArgumentException("Tablica nie może być pusta.", nameof(numbers));
-        double sum = 0;
-        foreach (var num in numbers)
-        {
-            sum += num;
-        }
-        return sum / numbers.Length;
+        total += num;
     }
+    return total / numbers.Length;
+}
+
 }
 
 public static class MathHelper
