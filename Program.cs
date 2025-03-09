ee71af0f05dd4920fbec2bf4b6931cb58dba2bf8
s30695 <s30695@pjwstk.edu.pl>
2025-03-09 22:54:31 +0100

    Dodanie statycznej metody MaxValue

--- Program.cs
+++ Program.cs
@@ -15,5 +15,24 @@ public static class MathHelper
     }
 }
 
+public static class MathHelper
+{
+    public static int MaxValue(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica nie może być pusta.", nameof(numbers));
+
+        int max = numbers[0];
+        foreach (var number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        return max;
+    }
+}
+
 
 Console.WriteLine("Hello, World!");
