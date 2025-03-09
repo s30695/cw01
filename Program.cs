18413f1e728a5d934216ccf7bd50ff7c7f015994
s30695 <s30695@pjwstk.edu.pl>
2025-03-09 22:52:11 +0100

    Dodanie statycznej metody CalculateAverage

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+public static class MathHelper
+{
+    public static double CalculateAverage(int[] numbers)
+    {
+        if (numbers == null || numbers.Length == 0)
+            throw new ArgumentException("Tablica nie może być pusta.", nameof(numbers));
+        double sum = 0;
+        foreach (var num in numbers)
+        {
+            sum += num;
+        }
+        return sum / numbers.Length;
+    }
+}
+
+
 Console.WriteLine("Hello, World!");
-//comm1
-//comm2
-//comm3
